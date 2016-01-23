ceced888ebffdb16322e837233d7836c9f63c864
ruthgrace <ruthgracewong@gmail.com>
2016-01-23 01:33:17 -0500

    fixed up nash script

--- nash_script.r
+++ nash_script.r
@@ -27,73 +27,48 @@ rownames(metadata) <- gsub("a$","",rownames(metadata))
 ## sanity check to make sure all your counts have metadata
 # which(!(colnames(otu.tab) %in% rownames(metadata)))
 
+otu.tab <- t(otu.tab)
+
 #rarefy data for unweighted unifrac
 otu.tab.rarefy <- rrarefy(otu.tab, min(apply(otu.tab,1,sum)))
 
-## input parameters for unweighted unifrac
-otuTable <- otu.tab.rarefy
-tree.original <- tree
-method="unweighted"
-verbose=TRUE
-pruneTree=FALSE
-normalize=TRUE
-
 #calculate distance matrix
+
+# do unweighted separately with rarefied data set
 unweighted <- getDistanceMatrix(otu.tab.rarefy,tree,method="unweighted",verbose=TRUE)
-weighted <- getDistanceMatrix(otu.tab,tree,method="weighted",verbose=TRUE)
-information <- getDistanceMatrix(otu.tab,tree,method="information",verbose=TRUE)
-exponent <- getDistanceMatrix(otu.tab,tree,method="exponent",verbose=TRUE)
+
+all_distance_matrices <- getDistanceMatrix(otu.tab,tree,method="all",verbose=TRUE)
+
+weighted <- all_distance_matrices[["weighted"]]
+information <- all_distance_matrices[["information"]]
+ratio <- all_distance_matrices[["ratio"]]
+ratio_no_log <- all_distance_matrices[["ratio_no_log"]]
 
 #output distance matrices
 write.table(unweighted,file="nash_output/unweighted_distance_matrix.txt",sep="\t",quote=FALSE)
 write.table(weighted,file="nash_output/weighted_distance_matrix.txt",sep="\t",quote=FALSE)
 write.table(information,file="nash_output/information_distance_matrix.txt",sep="\t",quote=FALSE)
-write.table(exponent,file="nash_output/exponent_normalize_distance_matrix.txt",sep="\t",quote=FALSE)
+write.table(ratio,file="nash_output/ratio_distance_matrix.txt",sep="\t",quote=FALSE)
+write.table(ratio_no_log,file="nash_output/ratio_no_log_distance_matrix.txt",sep="\t",quote=FALSE)
 
-#conditions (bv - bacterial vaginosis as scored by nugent/amsel, i - intermediate, n - normal/healthy)
-groups <- MyMetaOrdered$SSvsNASH
+# conditions: Originally 0 meant steatohepatosis, and 1 meant NASH
+groups <- metadata$SSvsNASH[match(rownames(otu.tab),rownames(metadata))]
 originalgroups <- groups
 
-# healthy is represented by 0, SS/NASH is represented by 1
+# Make healthy represented by 0, SS by 1, NASH by 2
 groups <- groups + 1;
-groups[which(is.na(MyMetaOrdered$SSvsNASH))] <- 0
+groups[which(is.na(groups))] <- 0
 
-# change conditions so that samples which are more than 50% one taxa are colored by that taxa
-otuSum <- apply(otu.tab,1,sum)
-otuMax <- apply(otu.tab,1,max)
-otuWhichMax <- apply(otu.tab,1,which.max)
-otuDominated <- which(otuMax > otuSum/2)
-
-
-otuMaxTax <- taxonomy[otuWhichMax]
-#otuDominated <- c(otuDominated[which(as.numeric(otuMaxTax[otuDominated])==32)],otuDominated[which(as.numeric(otuMaxTax[otuDominated])==33)])
-
-taxonomyGroups <- as.character(groups)
-taxonomyGroups[otuDominated] <- as.character(otuMaxTax[otuDominated])
-
-taxonomyGroups <- as.factor(taxonomyGroups)
-
-groups <- taxonomyGroups
-
-# assign appropriate names to single taxa dominated groups
-newLevels <- levels(taxonomyGroups)
-splittaxa <- strsplit(levels(taxonomyGroups),split=";")
-
-for (i in 1:length(splittaxa)) {
-	if (length(splittaxa[[i]])>1) {
-		newLevels[i] <- paste(splittaxa[[i]][length(splittaxa[[i]])-1],splittaxa[[i]][length(splittaxa[[i]])])
-	}
-	else {
-		newLevels[i] <- splittaxa[[i]][1]
-	}
-}
-
-levels(taxonomyGroups) <- newLevels
+# remove SS (intermediate group)
+otu.tab.original <- otu.tab
+otu.tab <- otu.tab[which(groups != 1),]
+groups <- groups[which(groups != 1)]
 
 unweighted.pcoa <- pcoa(unweighted)
 weighted.pcoa <- pcoa(weighted)
 information.pcoa <- pcoa(information)
-exponent.pcoa <- pcoa(exponent)
+ratio.pcoa <- pcoa(ratio)
+ratio_no_log.pcoa <- pcoa(ratio_no_log)
 
 
 #function to get variance explained for the PCOA component labels
@@ -108,30 +83,43 @@ getVarExplained <- function(vector) {
 unweighted.varEx <- getVarExplained(unweighted.pcoa$vectors)
 weighted.varEx <- getVarExplained(weighted.pcoa$vectors)
 information.varEx <- getVarExplained(information.pcoa$vectors)
-exponent.varEx <- getVarExplained(exponent.pcoa$vectors)
+ratio.varEx <- getVarExplained(ratio.pcoa$vectors)
+ratio_no_log.varEx <- getVarExplained(ratio_no_log.pcoa$vectors)
 
 #get vector version of distance matrices for correlation plots below
 unweighted.vector <- unlist(unweighted[lower.tri(unweighted,diag=TRUE)])
 weighted.vector <- unlist(weighted[lower.tri(weighted,diag=TRUE)])
 information.vector <- unlist(information[lower.tri(information,diag=TRUE)])
-exponent.vector <- unlist(exponent[lower.tri(exponent,diag=TRUE)])
+ratio.vector <- unlist(ratio[lower.tri(ratio,diag=TRUE)])
+ratio_no_log.vector <- unlist(ratio_no_log[lower.tri(ratio_no_log,diag=TRUE)])
 
+groups[which(groups==0)] <- "Healthy"
+groups[which(groups==2)] <- "NASH"
 
+groups <- as.factor(groups)
 
 pdf("nash_output/pcoa_plots.pdf")
+par(oma=c(1,1,1,5))
 
 #plot pcoa plots
 plot(unweighted.pcoa$vectors[,1],unweighted.pcoa$vectors[,2], col=groups,main="Unweighted UniFrac\nprincipal coordinates analysis",xlab=paste("First Component", round(unweighted.varEx[1],digits=3),"variance explained"),ylab=paste("Second Component", round(unweighted.varEx[2],digits=3),"variance explained"),pch=19,cex.lab=1.4,cex.main=2)
+legend("right", levels(groups), pch=c(19,19), col=palette()[c(1:2)], xpd=NA, inset=c(-0.25,0))
 plot(weighted.pcoa$vectors[,1],weighted.pcoa$vectors[,2], col=groups,main="Weighted UniFrac\nprincipal coordinates analysis",xlab=paste("First Component", round(weighted.varEx[1],digits=3),"variance explained"),ylab=paste("Second Component", round(weighted.varEx[2],digits=3),"variance explained"),pch=19,cex.lab=1.4,cex.main=2)
-legend(-0.37,-0.05,c("Healthy","SS","NASH"),col=palette(),pch=19)
+legend("right", levels(groups), pch=c(19,19), col=palette()[c(1:2)], xpd=NA, inset=c(-0.25,0))
 plot(information.pcoa$vectors[,1],information.pcoa$vectors[,2], col=groups,main="Information UniFrac\nprincipal coordinates analysis",xlab=paste("First Component", round(information.varEx[1],digits=3),"variance explained"),ylab=paste("Second Component", round(information.varEx[2],digits=3),"variance explained"),pch=19,cex.lab=1.4,cex.main=2)
-plot(exponent.pcoa$vectors[,1],exponent.pcoa$vectors[,2], col=groups,main="Exponent Normalized UniFrac\nprincipal coordinates analysis",xlab=paste("First Component", round(exponent.varEx[1],digits=3),"variance explained"),ylab=paste("Second Component", round(exponent.varEx[2],digits=3),"variance explained"),pch=19,cex.lab=1.4,cex.main=2)
+legend("right", levels(groups), pch=c(19,19), col=palette()[c(1:2)], xpd=NA, inset=c(-0.25,0))
+plot(ratio.pcoa$vectors[,1],ratio.pcoa$vectors[,2], col=groups,main="Ratio Normalized UniFrac\nprincipal coordinates analysis",xlab=paste("First Component", round(ratio.varEx[1],digits=3),"variance explained"),ylab=paste("Second Component", round(ratio.varEx[2],digits=3),"variance explained"),pch=19,cex.lab=1.4,cex.main=2)
+legend("right", levels(groups), pch=c(19,19), col=palette()[c(1:2)], xpd=NA, inset=c(-0.25,0))
+plot(ratio_no_log.pcoa$vectors[,1],ratio_no_log.pcoa$vectors[,2], col=groups,main="Ratio (no log) Normalized UniFrac\nprincipal coordinates analysis",xlab=paste("First Component", round(ratio_no_log.varEx[1],digits=3),"variance explained"),ylab=paste("Second Component", round(ratio_no_log.varEx[2],digits=3),"variance explained"),pch=19,cex.lab=1.4,cex.main=2)
+legend("right", levels(groups), pch=c(19,19), col=palette()[c(1:2)], xpd=NA, inset=c(-0.25,0))
 
 #plot correlation between different UniFrac modes
 plot(unweighted.vector,information.vector,main="unweighted vs. information UniFrac")
 plot(weighted.vector,information.vector,main="weighted vs. information UniFrac")
 plot(unweighted.vector,weighted.vector,main="unweighted vs. weighted UniFrac")
-plot(exponent.vector,information.vector,main="normalized exponent vs. information UniFrac")
-plot(exponent.vector,weighted.vector,main="normalized exponent vs. weighted UniFrac")
+plot(ratio.vector,information.vector,main="normalized ratio vs. information UniFrac")
+plot(ratio.vector,weighted.vector,main="normalized ratio vs. weighted UniFrac")
+plot(ratio_no_log.vector,information.vector,main="normalized ratio (no log) vs. information UniFrac")
+plot(ratio_no_log.vector,weighted.vector,main="normalized ratio (no log) vs. weighted UniFrac")
 
 dev.off()
