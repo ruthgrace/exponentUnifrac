285d2d53ac4fc7f7cac0e1aac87e816c2de5c3dd
ruthgrace <ruthgracewong@gmail.com>
2016-01-23 00:40:34 -0500

    updated gut saliva script

--- gut_saliva_script.r
+++ gut_saliva_script.r
@@ -13,7 +13,7 @@ getVarExplained <- function(vector) {
 	return(varEx)
 }
 
-plot_all_gut_saliva_unifrac <- function(count_file, tree_file, output_file) {
+plot_all_gut_saliva_unifrac <- function(count_file, tree_file, output_folder) {
 	source("UniFrac_memory_efficient.r")
 	library(ape)
 	library(phangorn)
@@ -40,30 +40,31 @@ plot_all_gut_saliva_unifrac <- function(count_file, tree_file, output_file) {
 	
 	unweighted <- all_distance_matrices[["unweighted"]]
 	#output distance matrices
-	write.table(unweighted,file="unweighted_distance_matrix.txt",sep="\t",quote=FALSE)
+	write.table(unweighted,file=paste(output_folder,"unweighted_distance_matrix.txt",sep=""),sep="\t",quote=FALSE)
 	weighted <- all_distance_matrices[["weighted"]]
-	write.table(weighted,file="weighted_distance_matrix.txt",sep="\t",quote=FALSE)
+	write.table(weighted,file=paste(output_folder,"weighted_distance_matrix.txt",sep=""),sep="\t",quote=FALSE)
 	information <- all_distance_matrices[["information"]]
-	write.table(information,file="information_distance_matrix.txt",sep="\t",quote=FALSE)
+	write.table(information,file=paste(output_folder,"information_distance_matrix.txt",sep=""),sep="\t",quote=FALSE)
 	ratio <- all_distance_matrices[["ratio"]]
-	write.table(ratio,file="ratio_normalize_distance_matrix.txt",sep="\t",quote=FALSE)
+	write.table(ratio,file=paste(output_folder,"ratio_normalize_distance_matrix.txt",sep=""),sep="\t",quote=FALSE)
 	ratio_no_log <- all_distance_matrices[["ratio_no_log"]]
-	write.table(ratio_no_log,file="ratio_no_log_normalize_distance_matrix.txt",sep="\t",quote=FALSE)
+	write.table(ratio_no_log,file=paste(output_folder,"ratio_no_log_normalize_distance_matrix.txt",sep=""),sep="\t",quote=FALSE)
 
+	# unweighted <- read.table("gut_saliva_output/low_sequencing_depth/unweighted_distance_matrix.txt",sep="\t",quote="",check.names=FALSE)
+	# weighted <- read.table("gut_saliva_output/low_sequencing_depth/weighted_distance_matrix.txt",sep="\t",quote="",check.names=FALSE)
+	# information <- read.table("gut_saliva_output/low_sequencing_depth/information_distance_matrix.txt",sep="\t",quote="",check.names=FALSE)
+	# ratio <- read.table("gut_saliva_output/low_sequencing_depth/ratio_normalize_distance_matrix.txt",sep="\t",quote="",check.names=FALSE)
+	# ratio_no_log <- read.table("gut_saliva_output/low_sequencing_depth/ratio_no_log_normalize_distance_matrix.txt",sep="\t",quote="",check.names=FALSE)
+	
 	#conditions (bv - bacterial vaginosis as scored by nugent/amsel, i - intermediate, n - normal/healthy)
-	groups <- MyMetaOrdered$SSvsNASH
+	groups <- MyMeta
 	originalgroups <- groups
-
-	# healthy is represented by 0, SS/NASH is represented by 1
-	groups <- groups + 1;
-	groups[which(is.na(MyMetaOrdered$SSvsNASH))] <- 0
 
 	# change conditions so that samples which are more than 50% one taxa are colored by that taxa
 	otuSum <- apply(otu.tab,1,sum)
 	otuMax <- apply(otu.tab,1,max)
 	otuWhichMax <- apply(otu.tab,1,which.max)
 	otuDominated <- which(otuMax > otuSum/2)
-
 
 	otuMaxTax <- taxonomy[otuWhichMax]
 	#otuDominated <- c(otuDominated[which(as.numeric(otuMaxTax[otuDominated])==32)],otuDominated[which(as.numeric(otuMaxTax[otuDominated])==33)])
@@ -109,17 +110,20 @@ plot_all_gut_saliva_unifrac <- function(count_file, tree_file, output_file) {
 	ratio.vector <- unlist(ratio[lower.tri(ratio,diag=TRUE)])
 	ratio_no_log.vector <- unlist(ratio_no_log[lower.tri(ratio_no_log,diag=TRUE)])
 
-
-
-	pdf("nash_output/pcoa_plots.pdf")
-
+	pdf(paste(output_folder,"pcoa_plots.pdf",sep=""))
+	# par(mar=c(3,3,3,2.1), oma=c(1,0,0,5))
+	par(oma=c(1,1,1,5),mar=(c(5, 4, 4, 2) + 0.1))
 	#plot pcoa plots
 	plot(unweighted.pcoa$vectors[,1],unweighted.pcoa$vectors[,2], col=groups,main="Unweighted UniFrac\nprincipal coordinates analysis",xlab=paste("First Component", round(unweighted.varEx[1],digits=3),"variance explained"),ylab=paste("Second Component", round(unweighted.varEx[2],digits=3),"variance explained"),pch=19,cex.lab=1.4,cex.main=2)
+	legend("right", levels(groups), pch=c(19,19), col=palette()[c(1:2)], xpd=NA, inset=c(-0.25,0))
 	plot(weighted.pcoa$vectors[,1],weighted.pcoa$vectors[,2], col=groups,main="Weighted UniFrac\nprincipal coordinates analysis",xlab=paste("First Component", round(weighted.varEx[1],digits=3),"variance explained"),ylab=paste("Second Component", round(weighted.varEx[2],digits=3),"variance explained"),pch=19,cex.lab=1.4,cex.main=2)
-	legend(-0.37,-0.05,c("Healthy","SS","NASH"),col=palette(),pch=19)
+	legend("right", levels(groups), pch=c(19,19), col=palette()[c(1:2)], xpd=NA, inset=c(-0.25,0))
 	plot(information.pcoa$vectors[,1],information.pcoa$vectors[,2], col=groups,main="Information UniFrac\nprincipal coordinates analysis",xlab=paste("First Component", round(information.varEx[1],digits=3),"variance explained"),ylab=paste("Second Component", round(information.varEx[2],digits=3),"variance explained"),pch=19,cex.lab=1.4,cex.main=2)
+	legend("right", levels(groups), pch=c(19,19), col=palette()[c(1:2)], xpd=NA, inset=c(-0.25,0))
 	plot(ratio.pcoa$vectors[,1],ratio.pcoa$vectors[,2], col=groups,main="ratio Normalized UniFrac\nprincipal coordinates analysis",xlab=paste("First Component", round(ratio.varEx[1],digits=3),"variance explained"),ylab=paste("Second Component", round(ratio.varEx[2],digits=3),"variance explained"),pch=19,cex.lab=1.4,cex.main=2)
+	legend("right", levels(groups), pch=c(19,19), col=palette()[c(1:2)], xpd=NA, inset=c(-0.25,0))
 	plot(ratio_no_log.pcoa$vectors[,1],ratio_no_log.pcoa$vectors[,2], col=groups,main="ratio no log Normalized UniFrac\nprincipal coordinates analysis",xlab=paste("First Component", round(ratio_no_log.varEx[1],digits=3),"variance explained"),ylab=paste("Second Component", round(ratio_no_log.varEx[2],digits=3),"variance explained"),pch=19,cex.lab=1.4,cex.main=2)
+	legend("right", levels(groups), pch=c(19,19), col=palette()[c(1:2)], xpd=NA, inset=c(-0.25,0))
 
 	#plot correlation between different UniFrac modes
 	plot(unweighted.vector,information.vector,main="unweighted vs. information UniFrac")
@@ -135,9 +139,9 @@ plot_all_gut_saliva_unifrac <- function(count_file, tree_file, output_file) {
 ## for testing/debugging purposes
 count_file <- "data/gut_saliva_data/low_sequencing_depth_hmp_data.txt"
 tree_file <- "data/gut_saliva_data/low_sequencing_depth_subtree.tre"
-output_file <- "gut_vs_saliva_sequencing_depth_less_than_3000.pdf"
+output_folder <- "gut_saliva_output/low_sequencing_depth/less_than_3000_readcounts_"
 
-plot_all_gut_saliva_unifrac("data/gut_saliva_data/low_sequencing_depth_hmp_data.txt", "data/gut_saliva_data/low_sequencing_depth_subtree.tre","gut_vs_saliva_sequencing_depth_less_than_3000.pdf")
-plot_all_gut_saliva_unifrac("data/gut_saliva_data/med_sequencing_depth_hmp_data.txt", "data/gut_saliva_data/med_sequencing_depth_subtree.tre","gut_vs_saliva_sequencing_depth_3000_to_6000.pdf")
-plot_all_gut_saliva_unifrac("data/gut_saliva_data/high_sequencing_depth_hmp_data.txt", "data/gut_saliva_data/high_sequencing_depth_subtree.tre","gut_vs_saliva_sequencing_depth_6000_plus.pdf")
+plot_all_gut_saliva_unifrac("data/gut_saliva_data/low_sequencing_depth_hmp_data.txt", "data/gut_saliva_data/low_sequencing_depth_subtree.tre","gut_saliva_output/low_sequencing_depth/less_than_3000_readcounts_")
+plot_all_gut_saliva_unifrac("data/gut_saliva_data/med_sequencing_depth_hmp_data.txt", "data/gut_saliva_data/med_sequencing_depth_subtree.tre","gut_saliva_output/low_sequencing_depth/3000_to_6000_readcounts_")
+plot_all_gut_saliva_unifrac("data/gut_saliva_data/high_sequencing_depth_hmp_data.txt", "data/gut_saliva_data/high_sequencing_depth_subtree.tre","gut_saliva_output/low_sequencing_depth/more_than_6000_readcounts_")
 
