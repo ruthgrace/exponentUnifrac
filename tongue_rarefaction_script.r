87ce62f766296f287ff605fa69dcdba22e864bb7
ruthgrace <ruthgracewong@gmail.com>
2016-01-29 12:21:16 -0500

    tongue rareafction

--- tongue_rarefaction_script.r
+++ tongue_rarefaction_script.r
@@ -14,16 +14,18 @@ tree <- read.tree("data/tongue_rarefaction_data/fasttree_all_seed_OTUs.tre")
 tree <- midpoint(tree)
 tree$tip.label <- gsub("'","",tree$tip.label)
 
-for (i in 51:101) {
+for (i in c(1:101)) {
   data <- read.table(paste("data/tongue_rarefaction_data/otus_rarefied_",i,".txt",sep=""),sep="\t",header=TRUE,check.names=FALSE,comment.char="",quote="",skip=1,row.names=1)
-  
   data <- t(data)
+    
+  tips <- tree$tip.label
+  tips <- tips[which(!(tree$tip.label %in% colnames(data)))]
+  new.tree <- tree
+  new.tree <- drop.tip(new.tree, tips)
   
-  all_distance_matrices <- getDistanceMatrix(data,tree,method="all",verbose=TRUE)
+  all_distance_matrices <- getDistanceMatrix(data,new.tree,method="all",verbose=TRUE)
   
-  information <- all_distance_matrices[["information"]]
-  write.table(information,file=paste("tongue_rarefaction_output/otus_rarefied_",i,"_information_distance_matrix.txt",sep=""),sep="\t",quote=FALSE)
-  ratio_no_log <- all_distance_matrices[["ratio_no_log"]]
-  write.table(ratio_no_log,file=paste("tongue_rarefaction_output/otus_rarefied_",i,"_ratio_no_log_normalize_distance_matrix.txt",sep=""),sep="\t",quote=FALSE)
+  ratio <- all_distance_matrices[["ratio"]]
+  write.table(ratio,file=paste("tongue_rarefaction_output/otus_rarefied_",i,"_ratio_distance_matrix.txt",sep=""),sep="\t",quote=FALSE)
   
 }
