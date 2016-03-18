0c823719ceecb391ace623d4e6566961c868f58b
ruthgrace <ruthgracewong@gmail.com>
2016-03-17 23:41:46 -0400

    added nafld effect size plots and data

--- nash_script.r
+++ nash_script.r
@@ -59,7 +59,7 @@ write.table(ratio_no_log,file="nash_output/ratio_no_log_distance_matrix.txt",sep
 # ratio_no_log <- read.table("nash_output/ratio_no_log_distance_matrix.txt", sep = "\t", quote = "", row.names = 1, check.names = FALSE)
 
 # conditions: Originally 0 meant steatohepatosis, and 1 meant NASH
-groups <- metadata$SSvsNASH[match(colnames(otu.tab),rownames(metadata))]
+groups <- metadata$SSvsNASH[match(rownames(otu.tab),rownames(metadata))]
 originalgroups <- groups
 
 # Make healthy represented by 0, SS by 1, NASH by 2
@@ -70,10 +70,10 @@ groups[which(is.na(groups))] <- 0
 groups <- groups + 1
 
 # mark healthy samples selected for metagenomic study
-groups[which(colnames(otu.tab) %in% metagenomic_samples & groups == 1)] <- 0
+groups[which(rownames(otu.tab) %in% metagenomic_samples & groups == 1)] <- 0
 
 # mark nash samples selected for metagenomic study
-groups[which(colnames(otu.tab) %in% metagenomic_samples & groups == 3)] <- 4
+groups[which(rownames(otu.tab) %in% metagenomic_samples & groups == 3)] <- 4
 
 # # remove SS (intermediate group)
 # otu.tab.original <- otu.tab
@@ -158,31 +158,49 @@ library(ALDEx2)
 
 pdf("nash_output/ALDEx_effect_size_plots.pdf")
 
-h.ss <- otu.tab
+h.ss <- t(otu.tab)
 h.ss.cond <- groups
 h.ss <- h.ss[,which(h.ss.cond == "Healthy Metagenomic" | h.ss.cond == "SS")]
 h.ss.cond <- h.ss.cond[which(h.ss.cond == "Healthy Metagenomic" | h.ss.cond == "SS")]
 
 h.ss.aldex <- aldex(data.frame(h.ss),as.character(h.ss.cond))
+aldex.plot(h.ss.aldex,type="MA",main="Bland-Altman style plot for healthy vs. SS",ylab="ratio",xlab="average")
+aldex.plot(h.ss.aldex,type="MW",main="Difference within vs. difference between for healthy vs. SS",xlab="Difference within",ylab="Difference between")
 
-h.nash <- otu.tab
+h.nash <- t(otu.tab)
 h.nash.cond <- groups
 h.nash.cond[which(h.nash.cond == "NASH Metagenomic")] <- "NASH"
 h.nash <- h.nash[,which(h.nash.cond == "Healthy Metagenomic" | h.nash.cond == "NASH")]
 h.nash.cond <- h.nash.cond[which(h.nash.cond == "Healthy Metagenomic" | h.nash.cond == "NASH")]
 
 h.nash.aldex <- aldex(data.frame(h.nash),as.character(h.nash.cond))
+aldex.plot(h.nash.aldex,type="MA",main="Bland-Altman style plot for healthy vs. NASH",ylab="ratio",xlab="average")
+aldex.plot(h.nash.aldex,type="MW",main="Difference within vs difference between for healthy vs. NASH",xlab="Difference within",ylab="Difference between")
 
-h.metnash <- otu.tab
+h.metnash <- t(otu.tab)
 h.metnash.cond <- groups
 h.metnash <- h.metnash[,which(h.metnash.cond == "Healthy Metagenomic" | h.metnash.cond == "NASH Metagenomic")]
 h.metnash.cond <- h.metnash.cond[which(h.metnash.cond == "Healthy Metagenomic" | h.metnash.cond == "NASH Metagenomic")]
 
 h.metnash.aldex <- aldex(data.frame(h.metnash),as.character(h.metnash.cond))
+aldex.plot(h.metnash.aldex,type="MA",main="Bland-Altman style plot for healthy vs. extreme NASH",ylab="ratio",xlab="average")
+aldex.plot(h.metnash.aldex,type="MW",main="Difference within vs difference between for healthy vs. extreme NASH",xlab="Difference within",ylab="Difference between")
 
-groups[which(groups == 0)] <- "Healthy Metagenomic"
-groups[which(groups == 1)] <- "Healthy"
-groups[which(groups == 2)] <- "SS"
-groups[which(groups == 3)] <- "NASH"
-groups[which(groups == 4)] <- "NASH Metagenomic"
+mycolor <- c(col2rgb("turquoise4"))
+red <- mycolor[1]
+green <- mycolor[2]
+blue <- mycolor[3]
+mycolor <- rgb(red/255, green/255, blue/255, 0.3)
 
+plot(h.ss.aldex$effect, h.nash.aldex$effect, pch=19,col=mycolor, main="Effect sizes of healthy vs SS compared to healthy vs NASH",xlab="Healthy vs. SS",ylab="Healthy vs. NASH")
+plot(h.ss.aldex$effect, h.metnash.aldex$effect, pch=19,col=mycolor, main="Effect sizes of healthy vs SS compared to healthy vs extreme NASH",xlab="Healthy vs. SS",ylab="Healthy vs. extreme NASH")
+plot(h.nash.aldex$effect, h.metnash.aldex$effect, pch=19,col=mycolor, main="Effect sizes of healthy vs NASH compared to healthy vs extreme NASH",xlab="Healthy vs. NASH",ylab="Healthy vs. extreme NASH")
+
+dev.off()
+
+print("Average difference in distance from zero of healthy vs. nash compared to healthy vs. ss:")
+summary(abs(h.nash.aldex$effect) - abs(h.ss.aldex$effect))
+print("Average difference in distance from zero of healthy vs. extreme nash compared to healthy vs. ss:")
+summary(abs(h.metnash.aldex$effect) - abs(h.ss.aldex$effect))
+print("Average difference in distance from zero of healthy vs. extreme nash compared to healthy vs. nash:")
+summary(abs(h.metnash.aldex$effect) - abs(h.nash.aldex$effect))
